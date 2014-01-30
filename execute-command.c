d803992b51c000b13bebec841ca87a806256a463
Leslie Wong <lwong60002000@gmail.com>
2014-01-29 18:56:27 -0800

    added comments and README

--- execute-command.c
+++ execute-command.c
@@ -35,7 +35,10 @@ execute_command (command_t c, int time_travel)
   c->input = NULL;
   c->output = NULL;*/
 
+	//since we have yet to implement time travel and time travel needs
+	//to be used, we put the current code into this conditional statement
 	if(time_travel == 0) {
+		//check that there is an actual command
 		if(c == NULL)
 			return;
 		
@@ -44,8 +47,10 @@ execute_command (command_t c, int time_travel)
 		int in, out;
 		int fd[2];
 		
+		//use switch statement to check command type and execute
+		//commands accordingly
 		switch(c->type){
-			//check which type of command it is
+			//for a && b, b only executes if a exists w/ zero
 			case AND_COMMAND:
 				execute_command(c->u.command[0],0);
 				if(c->u.command[0]->status != 0)
@@ -55,13 +60,15 @@ execute_command (command_t c, int time_travel)
 					c->status = c->u.command[1]->status;
 				}
 				break;
-	
+		
+			//just execute the left side and then the right side	
 			case SEQUENCE_COMMAND:
 				execute_command(c->u.command[0],0);
 				execute_command(c->u.command[1],0);
 				c->status = c->u.command[1]->status;
 				break;
 
+			//for a || b, b only executes if a exists w/ non-zero
 			case OR_COMMAND:
 				execute_command(c->u.command[0],0);
 				if(c->u.command[0]->status == 0)
@@ -72,6 +79,9 @@ execute_command (command_t c, int time_travel)
 				}
 				break;
 
+			//use fork initially so we can pipe and run one for the
+			//left side and one for the right side (requiring us to 
+			//fork another time inside)
 			case PIPE_COMMAND:
 				child = fork();
 				if (child < 0)
@@ -85,7 +95,7 @@ execute_command (command_t c, int time_travel)
 						close(fd[0]);
 						dup2(fd[1], 1);
 						close(fd[1]);
-						//printf("poop\n");
+						//printf("boop\n");
 						execute_command(c->u.command[0],0);
 						exit(c->u.command[0]->status); 
 					}
@@ -95,7 +105,7 @@ execute_command (command_t c, int time_travel)
 						close(fd[0]);
 						if (waitpid(grandchild, &p_status,0) == -1)
 							error(1,0, "Failure with G child process.");
-						//printf("pee\n");
+						//printf("beep\n");
 						execute_command(c->u.command[1],0);
 						c->u.command[0]->status = WEXITSTATUS(p_status);
 						exit(c->u.command[1]->status);
@@ -111,7 +121,11 @@ execute_command (command_t c, int time_travel)
 				break;
 					
 
+			//fork to run the simple command in another process
 			case SIMPLE_COMMAND:
+				//special case: exec, check for the word and then
+				//if exec is by itself, error and exit. If not, then
+				//run the command immediately following exec
 				if(strcmp(c->u.word[0],"exec") == 0) {
 					if(c->u.word[1] == '\0')
 						error(1,0,"No command following exec");
@@ -130,6 +144,7 @@ execute_command (command_t c, int time_travel)
 					execvp(c->u.word[1],c->u.word+1);
 					error(1,0,"Error when executing command2.");
 				}
+				//fork and check for redirections before executing the command
 				child = fork();
 				if(child < 0)
 					error(1,0,"Problems occurred during forking1.");
@@ -159,6 +174,9 @@ execute_command (command_t c, int time_travel)
 				}
 				break;
 
+			//This is basically the same as the last part of the SIMPLE_COMMAND
+			//without the special case for exec and the running of the subshell
+			//command through recursion.
 			case SUBSHELL_COMMAND:
 				child = fork();
 				if(child < 0)
@@ -176,7 +194,7 @@ execute_command (command_t c, int time_travel)
 					}
 					if(c->output != NULL) {
 						//if in here, there is a >
-						//printf("poop");
+						//printf("boop");
 						out = open(c->output, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
 						dup2(out,1);
 						close(out);
@@ -194,6 +212,7 @@ execute_command (command_t c, int time_travel)
 				}
 				break;
 
+			//If it is possibly anything thing else, there error 
 			default:
 				error(1,0,"Unknown/unsupported command type.");
 		}
